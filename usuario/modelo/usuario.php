60953b89f7832c9107ddb6b4c86ee3d2237f6c95
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-02-05 23:48:59 -0500

    Commit inicial

---
+++ usuario/modelo/usuario.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('../../core/BaseDatos/BDAbstract.php');
+
+class Usuario extends BDAbstract {
+    
+    public $nombre;
+    public $email;
+    protected $telefono_celular;
+    protected $cedula;
+    private $password;
+    
+    
+    public function insertar(){
+        
+    }
+    public function obtener(){
+        
+    }
+    public function actualizar(){
+        
+    }
+    public function eliminar(){
+        
+    }
+}
+?>
