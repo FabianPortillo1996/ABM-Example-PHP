ba24e1c27f526dd4df90481cdba0880248378311
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-02-05 22:16:43 -0500

    Agregado core base de datos

---
+++ core/BaseDatos/BDAbstract.php
@@ -0,0 +1,41 @@
+<?php
+abstract class BDAbstact{
+    
+    //Propiedades unicas de la clase
+    private static $bd_url = 'bd_url';
+    private static $bd_usuario = 'bd_usuario';
+    private static $bd_pass = 'bd_password';
+    private $conexion;
+    
+    //Propiedades que seran reescritas por clases hijas
+    protected $bd_nombre;
+    protected $filas = array();
+    protected $sentencia;
+    
+    abstract function insertar();
+    abstract function obtener();
+    abstract function actualizar();
+    abstract function eliminar();
+    
+    private function empezar_conexion(){
+        $this->conexion = new mysqli(self::bd_url, self::bd_usuario, self::bd_pass, $this->bd_nombre);
+    }
+    
+    private function cerrar_conexion(){
+        $this->conexion->close();
+    }
+    
+    protected function ejecutar_sentencia(){
+        $this->empezar_conexion();
+        $this->conexion->query($this->sentencia);
+        $this->cerrar_conexion();
+    }
+    
+    protected function obtener_datos(){
+        $this->empezar_conexion();
+        $resultados = $this->conexion->query($this->sentencia);
+        while($this->filas = $resultados->fetch_assoc());
+        $this->cerrar_conexion();
+    }
+}
+?>
