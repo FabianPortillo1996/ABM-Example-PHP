299a989feb1bfd2887495d6b376124e3facb7c4f
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-02-06 22:04:38 -0500

    Agregado el modelo de datos y sus funciones

--- usuario/modelo/usuario.php
+++ usuario/modelo/usuario.php
@@ -6,22 +6,52 @@ class Usuario extends BDAbstract {
     
     public $nombre;
     public $email;
-    protected $telefono_celular;
-    protected $cedula;
+    protected $telefono;
     private $password;
     
+    function __construct(){
+        $this->bd_nombre = 'veterinaria';
+    }
     
-    public function insertar(){
-        
+    public function insertar($datos_usuario = array()){
+        if(array_key_exists('email', $datos_usuario)){
+            $this->obtener($datos_usuario['email']);
+            if($datos_usuario != $this.email){
+                foreach($datos_usuario as $campo=>$valor){
+                    $campo = $valor;
+                }
+                $this->sentencia = "INSERT INTO usuarios (nombre,email,telefono,password) VALUES ('$nombre', '$email', '$telefono', '$password')";
+                $this->ejecutar_sentencia();
+            }
+        }
     }
-    public function obtener(){
-        
+    
+    
+    public function obtener($email = ''){
+        if($email != ''){
+            $this->sentencia = "SELECT * FROM usuarios WHERE email = '$email'";
+            $this->ejecutar_sentencia();
+        }
+        if(count($this->filas) == 1){
+            foreach($this->filas[0] as $propiedad => $valor){
+                $this->$propiedad = $valor;
+            }
+        }
     }
-    public function actualizar(){
-        
+    
+    
+    public function actualizar($datos_usuario = array()){
+        foreach($datos_usuario as $campo => $valor){
+            $campo = $valor;
+        }
+        $this->sentencia = "UPDATE usuarios SET nombre='$nombre',apellido='$apellido',telefono='$telefono' WHERE email='$email'";
+        $this->ejecutar_sentencia();
     }
-    public function eliminar(){
-        
+    
+    
+    public function eliminar($email = ''){
+        $this->sentencia = "DELETE FROM usuarios WHERE email='$email'";
+        $this->ejecutar_sentencia();
     }
 }
 ?>
